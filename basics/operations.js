2b849e57900ddf15d182db64d67d22e362f39bdd
MN Raza <mnraza94@gmail.com>
2024-12-15 04:20:51 +0530

    Operators

---
+++ basics/operations.js
@@ -0,0 +1,35 @@
+let num1 = 41
+let num2 = 20
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2); //remainder
+console.log(2**3);        //exponential
+
+// Comparison Operators
+
+let nums1 = 7
+let nums2 = 7
+let nums3 = 14
+
+console.log(nums1 == nums2);
+console.log(nums1 != nums2);
+console.log(nums1 > nums2);
+console.log(nums1 < nums2);
+
+
+// Logical Operators
+
+// &&  ||  !
+let isLoggedIn = true
+let isPaid = false
+
+console.log(isLoggedIn && isPaid);
+
+
+
+
+
+
