e6a11644214f0de4b98efa388658fab3af91672b
MN Raza <mnraza94@gmail.com>
2024-12-15 04:02:58 +0530

    console variants to print

---
+++ basics/print.js
@@ -0,0 +1,10 @@
+console.log("Hello there");
+process.stdout.write("Hi there");
+
+console.warn("It is warning msg");
+console.table({
+    name: 'MN Raza',
+    age: 29,
+    profession: 'Software Developer',
+    city: "Bhopal"
+})
