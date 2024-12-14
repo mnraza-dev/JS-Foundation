9b694bac0331299e954d10b0bd778326d353cb3f
MN Raza <mnraza94@gmail.com>
2024-12-15 04:36:52 +0530

    primitives

---
+++ basics/primitive.js
@@ -0,0 +1,22 @@
+/**
+ * Number
+ */
+let balance = 123;
+console.log(typeof balance);
+
+// boolean 
+let isActive = true
+
+// null & undefined 
+let firstname;
+console.log(firstname);
+
+let lastname = null
+console.log(lastname);
+
+// String
+
+let name = "MN Raza"
+let intro = `Hello! ${name}!`
+console.log(intro);
+
