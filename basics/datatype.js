3e13970c23ccd8f9338eca2a938da5ee51b81a9c
MN Raza <mnraza94@gmail.com>
2024-12-15 04:10:36 +0530

    Data Types

--- basics/datatype.js
+++ basics/datatype.js
@@ -4,8 +4,28 @@
  * 1. String
  * 2. Number
  * 3. Bigint
+ * 
  * 4. Undefined
  * 5. Null
+ * 
  * 6. Object
  * 7. Symbol
+ * 
  */
+
+// var score = 134;
+
+
+let score = 234; // Number
+let name = "MN Raza" // String
+let isLoggedIn = false  // Boolean
+
+// Object
+let person = {
+    name: "MN Raza",
+    age: 29
+}
+
+const username = "mnraza"
+ username = "raza"
+ console.log(username);
