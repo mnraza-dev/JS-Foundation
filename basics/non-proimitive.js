b98128efce0d03a0e450d3bbb9bc4d7dd8fae2b9
MN Raza <mnraza94@gmail.com>
2024-12-16 04:19:28 +0530

    Objects

---
+++ basics/non-proimitive.js
@@ -0,0 +1,14 @@
+const user = {
+    firstname: "MN",
+    lastname: "Raza",
+    email: 'mnraza94@gmail.com',
+    address: "Bhopal",
+    occupation: 'Softwrae Developer'
+}
+// console.log(user.firstname);
+// console.log(user['address']);
+
+user.age = 29
+
+console.log(user);
+
