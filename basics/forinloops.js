72858b9aeca634f0eba4dabf4e4bb0aa7276eeb3
MN Raza <mnraza94@gmail.com>
2024-12-16 23:52:24 +0530

    use of for of loop

---
+++ basics/forinloops.js
@@ -0,0 +1,12 @@
+const fruits = [
+    "Mangoes", "Apples", "Grapes", "Banana"];
+
+
+for (const item of fruits) {
+    console.log(item);
+}
+
+const username = "mnraza"
+for (const char of username) {
+    console.log(char);
+}
