25c0af7deb0c9b76e95bc57f821dad304e62adbc
MN Raza <mnraza94@gmail.com>
2024-12-16 23:56:30 +0530

    use of for in loop

--- basics/forinloops.js
+++ basics/forinloops.js
@@ -10,3 +10,24 @@ const username = "mnraza"
 for (const char of username) {
     console.log(char);
 }
+
+// to loop on ojects we should use for in loop
+const user = {
+    name: "MN Raza",
+    age: 29,
+    address: {
+        city: "Bhopal",
+        state: "MP"
+    }
+}
+
+for (const key in user) {
+    console.log(user[key]);
+}
+
+const userKeys = Object.keys(user)
+console.log(userKeys);
+
+for (const key of userKeys) {
+    console.log(user[key]);
+}
