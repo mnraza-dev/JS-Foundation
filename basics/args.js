d5dcc58af8da5e2accf08a5529f24bf32ff5a0d0
MN Raza <mnraza94@gmail.com>
2024-12-17 06:29:06 +0530

    arguments keyword won't work on arrow function

--- basics/args.js
+++ basics/args.js
@@ -8,3 +8,12 @@ function add() {
 console.log(add(1, 2,100,200));
 
 
+
+const add =(...args)=> {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum += args[i];
+    } return sum
+}
+
+console.log(add(1, 2,100,200));
