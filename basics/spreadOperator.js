829405d491dc65a9de531c7bcd7b62296402384e
MN Raza <mnraza94@gmail.com>
2024-12-17 06:43:08 +0530

    spread operator for Objects

--- basics/spreadOperator.js
+++ basics/spreadOperator.js
@@ -1,4 +1,5 @@
 // Spread Operator
+// with arrays
 const nums =[1,2,3,4,5]
 const newNums = [...nums, 6, 7, 8]
 console.log(newNums);
@@ -6,3 +7,11 @@ console.log(newNums);
 const myName = "MNRaza"
 const nameArr = [...myName]
 console.log(nameArr);
+// with Objects
+const person = {
+    name: "MN Raza",
+    age: 29
+}
+
+const personArr = {...person, city: "Bhopal"}
+console.log(personArr);
