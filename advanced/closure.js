ae6dcdc0464cc0ef660a41981f8d4870011ec1d2
MN Raza <mnraza94@gmail.com>
2024-12-20 01:53:38 +0530

    leant closures

---
+++ advanced/closure.js
@@ -0,0 +1,12 @@
+function outer() {
+    let counter = 0;
+    return function () {
+        counter++;
+        return counter;
+    }
+}
+
+const increment = outer();
+console.log(increment());
+console.log(increment());
+console.log(increment());   
