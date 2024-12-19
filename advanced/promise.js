83662e946089a1270e0e6842e94f562e6bcbe1a4
MN Raza <mnraza94@gmail.com>
2024-12-20 02:07:48 +0530

    learnt promises in JS

---
+++ advanced/promise.js
@@ -0,0 +1,32 @@
+// Promises are the utility to create asynchrounous nature in JavaScript    
+
+const fetchData = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let success = true;
+            if (success) {
+                resolve("Data Fetched")
+            }
+            else {
+                reject("Operation Rejected")
+            }
+
+        }, 3000);
+    })
+}
+
+
+fetchData()
+    .then((data) => {
+        console.log(data);
+        return {
+            name: "MN Raza",
+            age: 29,
+
+        }
+    }).then((val) => {
+        console.log("Done", val);
+    })
+    .catch((err) => {
+        console.error(err);
+    })
