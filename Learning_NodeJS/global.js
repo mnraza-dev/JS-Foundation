03c483567bc4749285b8e233b90212c15c9a733b
MN Raza <mnraza94@gmail.com>
2024-12-21 03:26:15 +0530

    Learnt about modules and exporting and importing them to use

--- Learning_NodeJS/global.js
+++ Learning_NodeJS/global.js
@@ -1,11 +1,14 @@
+const calculateSum = require('./sum.js')
+
 // console.log(global);
-
 // console.log(this); //empty object
-
 // console.log(globalThis); //global object
 // console.log(global === globalThis);
 
-console.log();
+let a = 24;
+let b = 45;
+
+console.log("the value of calculateSum is: ",calculateSum(a, b));
 
 
 
---
+++ Learning_NodeJS/sum.js
@@ -0,0 +1,7 @@
+// Modules protects their variables and functions from leaking 
+
+function calculateSum(x,y){
+    return x+y
+}
+
+module.exports = calculateSum
