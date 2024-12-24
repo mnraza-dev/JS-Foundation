1c4c373aabe902363495bf53c8e2ca6328ec0b6f
MN Raza <mnraza94@gmail.com>
2024-12-24 06:27:04 +0530

    Polyfill for map

--- interview_preparation/1-javascript-interview/2-map-filter-reduce/script.js
+++ interview_preparation/1-javascript-interview/2-map-filter-reduce/script.js
@@ -1,6 +1,6 @@
 // map
 
-const nums = [1, 2, 3, 4, 5, 6,7,8,9,10];
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const newNums = nums.map((num, index) => num + index);
 
@@ -15,9 +15,26 @@ console.log(moreThanTwo);
 // reduce method is used to reduce an array to a single value
 // accumulator is a variable that stores the result of the previous iteration/computation
 
-
 const sum = nums.reduce((acc, current, index, arr) => {
     return acc + current;
 }, 0);
 console.log(sum);
+
+// Polifill for map
+// Array.map((num, index, arr) => {num + index});
+
+Array.prototype.myMap = function (cb) {
+    let arr = [];
+    for (let i = 0; i < this.length; i++) {
+        arr.push(cb(this[i], i, this));
+    }
+    return arr;
+
+}
+
+const mynums = [123,124,245,567,367,873,783,567];
+
+const myNewNums = mynums.myMap((num, index) => num + index);
+
+console.log(myNewNums);
 
