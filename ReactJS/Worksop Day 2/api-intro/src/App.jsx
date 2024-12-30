3cd4f4cf40336efc68ad9a12fcd15a2d20708d65
MN Raza <mnraza94@gmail.com>
2024-12-31 04:41:56 +0530

    addeda feature of displaying random users on click of spacebar

--- ReactJS/Worksop Day 2/api-intro/src/App.jsx
+++ ReactJS/Worksop Day 2/api-intro/src/App.jsx
@@ -1,20 +1,59 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 const App = () => {
-  const [userDetails, setUserDetails] = useState({});
+  const [userDetails, setUserDetails] = useState(null);
+
+  const user = async () => {
+    try {
+      const userDetail = await axios.get('https://randomuser.me/api/');
+      setUserDetails(userDetail.data.results[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Event listener for spacebar press
+  useEffect(() => {
+    const handleSpacebarPress = (e) => {
+      if (e.key === " ") {
+        user();  
+      }
+    };
+
+    window.addEventListener('keydown', handleSpacebarPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleSpacebarPress);
+    };
+  }, []); 
 
   useEffect(() => {
-   try {
-    const userDetail = axios.get('https://randomuser.me/api/');
-    
-   } catch (error) {
-    console.log(error);
-    
-   } 
-  },[])
-  return (
-    <div>App</div>
-  )
-}
+    user(); 
+  }, []); 
 
-export default App
+  return (
+    <div className='user_Wrapper'>
+      <h1>User Details</h1>
+      <div className='user'>
+        {userDetails && (
+          <>
+            <img src={userDetails.picture.large} alt="User" width={300} style={{
+              objectFit: 'contain',
+              borderRadius: '12px'
+            }} />
+            <p>{userDetails.name.first} {userDetails.name.last}</p>
+            <p>Email: {userDetails.email}</p>
+            <p>Phone: {userDetails.phone}</p>
+            <p>City: {userDetails.location.city}</p>
+            <p>Country: {userDetails.location.country}</p>
+            <p>Timezone: {userDetails.location.timezone.description}</p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
