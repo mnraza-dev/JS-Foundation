ae18741094b896a392e73a3499b442995fb2a61e
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-08 10:30:51 +0530

    initialize Redux project with basic HTML, package.json, and store setup

---
+++ Redux/script.js
@@ -0,0 +1,17 @@
+import { createStore } from "redux";
+
+const store = createStore({
+    state: {
+        count: 0
+    },
+    reducers: {
+        increment: (state) => {
+            state.count += 1;
+        },
+        decrement: (state) => {
+            state.count -= 1;
+        }
+    }
+});
+
+export default store;
