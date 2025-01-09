e243161d3db59607283a0198d3526a4c1fd440c6
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-09 15:06:22 +0530

    add input field for incrementing count by user-defined amount in App component

--- Redux/RTK_in_ReactJS/src/App.jsx
+++ Redux/RTK_in_ReactJS/src/App.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { decrement, increment, incrementByAmount } from './redux/counterSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 const App = () => {
   const count = useSelector((state) => state.counter.value)
   const dispatch = useDispatch()
+  const inputRef = useRef();
+
   return (
     <div>
-      <h1>RTK - Redux Toolkit</h1>
+      <h1>RTK - Redux Toolkit || Counter Example</h1>
       <h3>Count: {count} </h3>
       <button onClick={() => dispatch(increment())} >Increment</button>
       <button onClick={() => dispatch(decrement())} >Decrement</button>
-      <button onClick={() => dispatch(incrementByAmount(6))} >Increment By Amount</button>
+      <div> <input type="text" name="count" id="" placeholder='Increment By Amount ' ref={inputRef} />
+        <button onClick={() => dispatch(incrementByAmount(parseInt(inputRef.current.value)))} >Increment By Amount</button></div>
     </div>
   )
 }
