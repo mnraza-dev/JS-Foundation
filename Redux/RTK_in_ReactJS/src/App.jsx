8a8a6006a7269c02e28b7617a1426d8c15254279
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-09 14:59:59 +0530

    implement Redux store and counter slice; update App component for state management and center layout

--- Redux/RTK_in_ReactJS/src/App.jsx
+++ Redux/RTK_in_ReactJS/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { decrement, increment, incrementByAmount } from './redux/counterSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>RTK - Redux Toolkit</h1>
+      <h3>Count: {count} </h3>
+      <button onClick={() => dispatch(increment())} >Increment</button>
+      <button onClick={() => dispatch(decrement())} >Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(6))} >Increment By Amount</button>
+    </div>
   )
 }
 
