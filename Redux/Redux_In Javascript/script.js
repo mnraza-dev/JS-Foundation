704b22cf7c2cb78ed0bd661e95243029c7bc8bde
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-11 00:13:37 +0530

    refactor reducer to use switch statement for action handling and clean up commented code

--- Redux/Redux_In Javascript/script.js
+++ Redux/Redux_In Javascript/script.js
@@ -7,20 +7,32 @@ const initialState = {
 };
 
 function reducer(state = initialState, action) {
-  if (action.type === "post/increment") {
-    return { ...state, count: (state.count += 1) };
-  } else if (action.type === "post/decrement") {
-    return { ...state, count: (state.count -= 1) };
-  } else if (action.type === "post/incrementByAmount") {
-    return { ...state, count: (state.count += action.payload) };
-  } else if (action.type === "post/deceremntByAmount") {
-    return { ...state, count: (state.count -= action.payload) };
-  } else if (action.type === "post/reset") {
-    return { ...state, count: 0 };
+  switch (action.type) {
+    case "post/increment":
+      return { ...state, count: (state.count += 1) };
+    case "post/decrement":
+      return { ...state, count: (state.count -= 1) };
+    case "post/incrementByAmount":
+      return { ...state, count: (state.count += action.payload) };
+    case "post/deceremntByAmount":
+      return { ...state, count: (state.count -= action.payload) };
+    case "post/reset":
+      return { ...state, count: 0 };
   }
-
-  return state;
 }
+//   if (action.type === "post/increment") {
+//     return { ...state, count: (state.count += 1) };
+//   } else if (action.type === "post/decrement") {
+//     return { ...state, count: (state.count -= 1) };
+//   } else if (action.type === "post/incrementByAmount") {
+//     return { ...state, count: (state.count += action.payload) };
+//   } else if (action.type === "post/deceremntByAmount") {
+//     return { ...state, count: (state.count -= action.payload) };
+//   } else if (action.type === "post/reset") {
+//     return { ...state, count: 0 };
+//   }
+
+//   return state;
 
 const store = createStore(reducer);
 
@@ -29,8 +41,9 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({ type: "post/increment" });
-// store.dispatch({ type: "post/decrement" });
-store.dispatch({ type: "post/increment" });
+// store.dispatch({ type: "post/increment" });
+store.dispatch({ type: "post/decrement" });
+// store.dispatch({ type: "post/increment" });
 store.dispatch({ type: "post/incrementByAmount", payload: 25 });
+store.dispatch({ type: "post/deceremntByAmount", payload: 5 });
 store.dispatch({ type: "post/deceremntByAmount", payload: 5 });
