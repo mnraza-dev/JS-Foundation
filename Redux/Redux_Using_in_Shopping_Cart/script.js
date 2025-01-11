bc59642fc1dfe65fddb30b9671e7541ee63208c9
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-11 08:51:28 +0530

    implement cart item addition in reducer and update logging in script

--- Redux/Redux_Using_in_Shopping_Cart/script.js
+++ Redux/Redux_Using_in_Shopping_Cart/script.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import {productList} from "./productList";
+import { productList } from "./productList";
 
 const initialState = {
   products: productList,
@@ -7,12 +7,26 @@ const initialState = {
   wishlist: [],
 };
 
-function reducer(state = initialState, action) {}
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "cart/addItem":
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
+      };
+  }
+}
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-console.log(store);
+// console.log(store);
+
+store.dispatch({
+  type: "cart/addItem",
+  payload: { productId: 1, quantity: 1 },
+});
+console.log("Fist get state ", store.getState());
 
 export default store;
