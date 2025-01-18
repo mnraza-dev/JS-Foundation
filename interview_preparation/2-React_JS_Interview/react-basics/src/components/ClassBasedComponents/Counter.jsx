c6bf66b95cee259485fc4521d34fecc404ce23b3
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-19 00:19:52 +0530

    Add increment functionality to Counter component and enhance logging

--- interview_preparation/2-React_JS_Interview/react-basics/src/components/ClassBasedComponents/Counter.jsx
+++ interview_preparation/2-React_JS_Interview/react-basics/src/components/ClassBasedComponents/Counter.jsx
@@ -7,15 +7,48 @@ class Counter extends React.Component {
       count: 0,
     };
   }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  componentDidMount() {
+    console.log("Component mounted");
+    console.log("when component is loaded, this method is called");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Component updated");
+  }
+
   render() {
     return (
-      <div style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh"
-      }}>
-        <h1>{this.state.count}</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <h1>Count: {this.state.count}</h1>
+        <button
+          style={{
+            marginLeft: "10px",
+            cursor: "pointer",
+            padding: "10px 20px",
+            fontSize: "16px",
+            backgroundColor: "blue",
+            color: "white",
+            border: "none",
+            borderRadius: "12px",
+          }}
+          onClick={this.incrementCount}
+        >
+          Increment Count
+        </button>
       </div>
     );
   }
