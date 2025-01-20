112115b8fb5ceb7f40f1127651502be405981ea4
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-21 05:08:23 +0530

    Add increment by functionality to useReducer example in App component

--- ReactJS/Complete_ReactJS/ReducerExample/src/App.jsx
+++ ReactJS/Complete_ReactJS/ReducerExample/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 const initialState = {
   count: 0,
 };
@@ -20,13 +20,19 @@ const reducer = (state, action) => {
         ...state,
         count: 0,
       };
+    case "ïncrementBy":
+      return {
+        ...state,
+        count: state.count + incrementBy,
+      };
 
-      default:
-        return state;
+    default:
+      return state;
   }
 };
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [incrementBy, setIncrementBy] = useState(0);
 
   return (
     <div>
@@ -44,6 +50,25 @@ const App = () => {
         But it is designed for complex state objects , It allows you to manage
         state in a functional, immutable way
       </h2>
+
+      <h1>Count : {state.count}</h1>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+      <h2>Increment By Example</h2>
+      <input
+        type="text"
+        name="incCount"
+        placeholder="Increment By"
+        onChange={(e) => setIncrementBy(e.target.value)}
+      />
+      <button
+        onClick={() => dispatch({ type: "incrementBy", payload: incrementBy })}
+      >
+        Increment By {incrementBy}
+      </button>
+
+      
     </div>
   );
 };
