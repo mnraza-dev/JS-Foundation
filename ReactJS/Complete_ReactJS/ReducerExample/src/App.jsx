b6598364bd942ff76c3ce7bebfb40cd07b2f7cae
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-21 05:01:29 +0530

    Implement useReducer for state management in App component

--- ReactJS/Complete_ReactJS/ReducerExample/src/App.jsx
+++ ReactJS/Complete_ReactJS/ReducerExample/src/App.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useReducer } from "react";
+const initialState = {
+  count: 0,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "decrement":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
 
+    case "reset":
+      return {
+        ...state,
+        count: 0,
+      };
+
+      default:
+        return state;
+  }
+};
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
-      <h1> <span style={{
-        color: "green"
-      }}>useReducer</span> is a hook that is similar to useState</h1>
+      <h1>
+        <span
+          style={{
+            color: "green",
+          }}
+        >
+          useReducer
+        </span>
+        is a hook that is similar to useState
+      </h1>
       <h2>
         But it is designed for complex state objects , It allows you to manage
         state in a functional, immutable way
