c9b94c2606932fee63e644e900930155f92c7615
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-21 04:47:05 +0530

    using useReducer() hook

---
+++ ReactJS/Complete_ReactJS/ReducerExample/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1> <span style={{
+        color: "green"
+      }}>useReducer</span> is a hook that is similar to useState</h1>
+      <h2>
+        But it is designed for complex state objects , It allows you to manage
+        state in a functional, immutable way
+      </h2>
+    </div>
+  );
+};
+
+export default App;
