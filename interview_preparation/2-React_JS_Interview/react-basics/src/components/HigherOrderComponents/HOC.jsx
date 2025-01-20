07504c7547c8c54e537e2591b2fb5cb973a6c9b4
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-20 08:11:05 +0530

    Add Higher Order Components (HOC) component with documentation

---
+++ interview_preparation/2-React_JS_Interview/react-basics/src/components/HigherOrderComponents/HOC.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const HOC = () => {
+
+    
+  return (
+    <div>
+        <h1>Higher Order Components</h1>
+{
+    /**
+     * Q.1 What are HOC ? 
+     * 
+     * - Function that takes a component and returns a new enhanced component 
+     * - Used for code reusability, logic sharing and adding extra features/functionality to components.
+     * 
+     * 
+     * 
+     *
+     * 
+     */
+}
+        
+    </div>
+  )
+}
+
+export default HOC
