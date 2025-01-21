974cc068b47d1c13ba605534152811660de7155a
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-21 06:57:27 +0530

    Add Button component and integrate it into App component

--- ReactJS/Complete_ReactJS/TypeScript Demos/src/App.tsx
+++ ReactJS/Complete_ReactJS/TypeScript Demos/src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react"
 import User from "./components/User"
+import Button from "./components/Button"
+
 const App: React.FC = () => {
   return (
     <div>
       <User name="John Doe" age={30} isStudent={true} />
+
+      <Button label="Click me" onClick={() => console.log("Button clicked")} disabled={true} />
     </div>
   )
 }
