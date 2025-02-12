703e8963b1122b884f85fcbbbc5561b4fe527765
mnraza-dev <mnraza94@gmail.com>
2025-02-12 16:20:00 +0530

    added more examples on Spread Operator

--- basics/spreadOperator.js
+++ basics/spreadOperator.js
@@ -1,9 +1,13 @@
 // Spread Operator
 // with arrays
+
 const nums =[1,2,3,4,5]
 const newNums = [...nums, 6, 7, 8]
+
 console.log(newNums);
+
 // with strings
+
 const myName = "MNRaza"
 const nameArr = [...myName]
 console.log(nameArr);
@@ -15,3 +19,17 @@ const person = {
 
 const personArr = {...person, city: "Bhopal"}
 console.log(personArr);
+
+
+// more examples from prepCoding
+
+const myArr = [2,3,4,4,5,5,6,8];
+const myNewArr = [...myArr];
+
+console.log(myNewArr);
+console.log(myArr);
+
+const obj1 = {name: "John", age: 25};
+const obj2 = {...obj1, city: "Bhopal", age:30};
+console.log(obj2);
+
