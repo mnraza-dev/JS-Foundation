169b505b580b473de7744edfb568725a1592f75c
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-17 03:57:56 +0530

    ✅useRef() hook - Usecase here is  to focus input on mount

--- ReactJS/learning-all-hooks/src/App.jsx
+++ ReactJS/learning-all-hooks/src/App.jsx
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import HookExample from "./hook-example";
+import { useRef } from "react";
 
 const App = () => {
   const [inputValue, setInputValue] = useState("");
   const [toggle, setToggle] = useState(false);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   return (
     <div className="App">
       <h1>Learning all hooks</h1>
       <input
+        ref={inputRef}
         type="text"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
