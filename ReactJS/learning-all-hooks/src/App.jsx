47815b0269368b39c47187f2f3cbd9c4b5ecb9f3
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-17 03:14:20 +0530

    ✅ useState() hook

--- ReactJS/learning-all-hooks/src/App.jsx
+++ ReactJS/learning-all-hooks/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
   return (
-    <div>App</div>
-  )
-}
+    <div className="App">
+      <h1>Learning all hooks</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <p>
+        You Typed: <strong>{inputValue}</strong>
+      </p>
+    </div>
+  );
+};
 
-export default App
+export default App;
