77d6b2f585ab87c6c5fb54774b1a2e41aecd52ad
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-17 19:53:13 +0530

    ✨ set up Redux store and integrate with App component

--- Redux/habbit-tracker/src/App.tsx
+++ Redux/habbit-tracker/src/App.tsx
@@ -1,8 +1,12 @@
+import { Provider } from "react-redux"
+import store from "./redux/store"
+
 const App = () => {
+
   return (
-    <div>
+    <Provider store={store}>
       <h1>App</h1>
-    </div>
+    </Provider>
   )
 }
 export default App
