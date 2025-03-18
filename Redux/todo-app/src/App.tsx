b9061d769f190d493f6e0a275f842e6982b47acf
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-18 23:45:50 +0530

    ✨ implement todo list functionality with add, toggle, and delete features in App component

--- Redux/todo-app/src/App.tsx
+++ Redux/todo-app/src/App.tsx
@@ -1,11 +1,67 @@
-import React from 'react'
+// src/App.tsx
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo, toggleTodo, deleteTodo } from './redux/todoSlice';
+import { RootState, AppDispatch } from './redux/store';
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
-}
+  const [input, setInput] = useState<string>(''); 
+  const todos = useSelector((state: RootState) => state.todos.items); 
+  const dispatch = useDispatch<AppDispatch>(); 
 
-export default App
+  const handleAddTodo = () => {
+    if (input.trim()) {
+      dispatch(addTodo(input));
+      setInput('');
+    }
+  };
+
+  return (
+    <div style={{ padding: '20px', maxWidth: '400px', margin: '0 auto' }}>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
+          placeholder="Add a new todo"
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              margin: '10px 0',
+            }}
+          >
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => dispatch(toggleTodo(todo.id))}
+            />
+            <span
+              style={{
+                marginLeft: '10px',
+                textDecoration: todo.completed ? 'line-through' : 'none',
+              }}
+            >
+              {todo.text}
+            </span>
+            <button
+              onClick={() => dispatch(deleteTodo(todo.id))}
+              style={{ marginLeft: 'auto', color: 'red' }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
