7989341c115fd81ab204f0321ac5631ec5b0b5fd
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-18 23:47:31 +0530

    ✨ update Redux store configuration to export AppDispatch type and improve code formatting

--- Redux/todo-app/src/redux/store.ts
+++ Redux/todo-app/src/redux/store.ts
@@ -1,20 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "./todoSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from './todoSlice';
+
 
 export interface RootState {
     todos: {
         items: Todo[];
     };
 }
+
 export interface Todo {
     id: number;
     text: string;
     completed: boolean;
 }
+
 const store = configureStore({
     reducer: {
         todos: todoReducer,
     },
-})
+});
 
-export default store
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
