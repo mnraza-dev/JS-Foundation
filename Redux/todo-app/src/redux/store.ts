fd9a4ad667afe593873e8a4d7985a179b5f86ea4
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-18 23:51:21 +0530

    ✨ refactor store.ts for improved code formatting and consistency in interface definitions

--- Redux/todo-app/src/redux/store.ts
+++ Redux/todo-app/src/redux/store.ts
@@ -1,23 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from './todoSlice';
 
-
 export interface RootState {
-    todos: {
-        items: Todo[];
-    };
+  todos: {
+    items: Todo[];
+  };
 }
 
 export interface Todo {
-    id: number;
-    text: string;
-    completed: boolean;
+  id: number;
+  text: string;
+  completed: boolean;
 }
 
 const store = configureStore({
-    reducer: {
-        todos: todoReducer,
-    },
+  reducer: {
+    todos: todoReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
