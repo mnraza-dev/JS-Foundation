5708551ede5ecc01ae7eb422f9473303cd6eeb5b
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-30 07:34:18 +0530

    ✨ add input box and button for todo entry in App component, enhance App.test.js with input box and button tests

--- Testing/blog-app/src/App.jsx
+++ Testing/blog-app/src/App.jsx
@@ -6,12 +6,16 @@ const App = () => {
       <h1>Hello World</h1>
 
       <img
-      title="Google Logo"
+        title="Google Logo"
         src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png"
         alt="Logo"
         width={200}
         height={100}
       />
+
+      <input type="text" name="todo" id="todo" placeholder="Enter a todo" /> <br />
+
+      <button type="button">Add Todo</button>
     </div>
   );
 };
--- Testing/blog-app/src/App.test.js
+++ Testing/blog-app/src/App.test.js
@@ -10,3 +10,17 @@ test("test for first react app", () => {
   expect(titleOfImage).toBeInTheDocument();
   expect(text).toBeInTheDocument();
 });
+
+test("Testing Input Box", () => {
+  render(<App />);
+  const inputBox = screen.getByRole("textbox");
+  const button = screen.getByRole("button");
+  const btnText = screen.getByText(/Add Todo/i);
+  const placeHolderText = screen.getByPlaceholderText(/Enter a todo/i);
+  expect(button).toBeInTheDocument();
+  expect(inputBox).toBeInTheDocument();
+  expect(inputBox).toHaveAttribute("name", "todo");
+  expect(inputBox).toHaveAttribute("id", "todo");
+  expect(placeHolderText).toBeInTheDocument();
+  expect(btnText).toBeInTheDocument();
+})
