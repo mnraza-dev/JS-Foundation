b9b5dfeb6cb5178d343ba371f894c6799e210ffd
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-09 21:54:18 +0530

    ✨ remove unnecessary whitespace in palindrome.js for improved code cleanliness

--- dsa_in_js/fibonacci.js
+++ dsa_in_js/fibonacci.js
@@ -1,1 +1,17 @@
 // WAP to check if a numeric value is fibonacci or not
+
+//  Fibbonacci series: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987
+
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// F(2) = F(1) + F(0) = 1 + 0 = 1
+// F(3) = F(2) + F(1) = 1 + 1 = 2
+
+var fib = function(n) {
+    const arr = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr[n];
+};
+console.log(fib(6)); 
--- dsa_in_js/palindrome.js
+++ dsa_in_js/palindrome.js
@@ -1,5 +1,4 @@
 // WAP to check if a numeric value is palindrome or not
-
 /**
  * 121 => true
  * 123 => false
