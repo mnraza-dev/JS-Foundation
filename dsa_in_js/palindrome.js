592a190e6e20ce4b0575e846993ef730a3a997f4
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-09 21:29:20 +0530

    ✨ refactor palindrome function to improve readability and handle negative numbers

--- dsa_in_js/palindrome.js
+++ dsa_in_js/palindrome.js
@@ -1,9 +1,8 @@
 // WAP to check if a numeric value is palindrome or not
 
 function palindrome(num) {
-    return num == + num.toString().split("").reverse().join("");
-
+  return num < 0 ? false : num === +num.toString().split("").reverse().join("");
 }
 
-const res = palindrome(1221);
+const res = palindrome(121);
 console.log(res);
