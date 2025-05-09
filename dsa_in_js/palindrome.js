beed5426ab4441b0e3454398ebc7e0f4bb92c3ff
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-09 21:30:23 +0530

    ✨ add detailed examples to palindrome function documentation

--- dsa_in_js/palindrome.js
+++ dsa_in_js/palindrome.js
@@ -1,5 +1,11 @@
 // WAP to check if a numeric value is palindrome or not
 
+/**
+ * 121 => true
+ * 123 => false
+ * -121 => false
+ * 10 => false 
+ */
 function palindrome(num) {
   return num < 0 ? false : num === +num.toString().split("").reverse().join("");
 }
