7b5d7d89efcf088249373a27ed36d82e84832dc6
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-10 22:08:29 +0530

    ✨ add isAnagram function to check if two strings are anagrams and implement removeDuplicates function to filter unique values from an array

---
+++ dsa_in_js/Remove-Duplicates-from-Array/index.js
@@ -0,0 +1,11 @@
+function removeDuplicates(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (result.indexOf(arr[i]) === -1) {
+        result.push(arr[i]);
+      }
+    }
+    return result;
+  }
+  console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
+  
