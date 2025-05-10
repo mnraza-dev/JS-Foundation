9c4b23250905351778bee881d699ed5010adfe05
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-10 22:02:20 +0530

    ✨ add findLargest function to determine the largest number in an array

---
+++ dsa_in_js/findLargestNumberInArray.js
@@ -0,0 +1,12 @@
+function findLargest(arr) {
+  if (arr.length === 0) return undefined;
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+console.log(findLargest([3, 1, 7, 4, 9, 2])); // Output: 9
