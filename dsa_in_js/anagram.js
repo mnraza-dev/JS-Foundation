95998b4021ae7e0fe147ad62296bf7f74a212d69
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-10 21:52:27 +0530

    refactor isAnagram function by removing unnecessary comments for clarity

--- dsa_in_js/anagram.js
+++ dsa_in_js/anagram.js
@@ -1,9 +1,8 @@
 // WAP to check if given string is anagam or not
-
 // An anagram of a string is another string that contains the same characters, only the order of the characters can be different. For example, "listen" and "silent" are anagrams of each other.
-
 // isAnagram("anagram", "nagaram") => true
 // isAnagram("rat", "car") => false
+
 var isAnagram = function (s, t) {
   return s.split("").sort().join("") === t.split("").sort().join("");
 };
