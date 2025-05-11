61ee9339af8d9a885b776ca37b6523db564d8b7f
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-11 17:42:09 +0530

    ✨ standardize comments for array operations and enhance readability

--- dsa_in_js/Data-Structures/Arrays/arrays-basics.js
+++ dsa_in_js/Data-Structures/Arrays/arrays-basics.js
@@ -1,12 +1,26 @@
-// Arrays declaration
+// Arrays- Declaration
 let arr = ["apple", "banana", "cherry"];
 let arr1 = new Array("apple", "banana", "cherry");
 let arr3 = new Array(5); 
 let arr4 = new Array();
 
-// Accessing elements
+// Array- Accessing Elements
+
 console.log(arr[0]); 
 console.log(arr[1]); 
 arr.map((item, index) => {
   console.log(item, index);
 });
+
+// Array- Length
+
+console.log(arr.length);
+
+// Array- Adding & Removing Elements
+
+arr.push("orange"); // Adds to the end
+arr.unshift("kiwi"); // Adds to the beginning
+arr.pop(); // Removes from the end
+arr.shift(); // Removes from the beginning
+arr.splice(1, 1); // Removes from the middle
+arr.splice(1, 0, "grape"); // Adds to the middle
