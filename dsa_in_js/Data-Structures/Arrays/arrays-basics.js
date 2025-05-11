7306be0c41ef2c60ac93ccf89acd4181e417ad0c
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-11 17:52:58 +0530

    refactor array declaration and access examples for improved consistency and readability

--- dsa_in_js/Data-Structures/Arrays/arrays-basics.js
+++ dsa_in_js/Data-Structures/Arrays/arrays-basics.js
@@ -1,16 +1,14 @@
 // Arrays- Declaration
+
 let arr = ["apple", "banana", "cherry"];
 let arr1 = new Array("apple", "banana", "cherry");
-let arr3 = new Array(5); 
+let arr3 = new Array(5);
 let arr4 = new Array();
 
 // Array- Accessing Elements
 
-console.log(arr[0]); 
-console.log(arr[1]); 
-arr.map((item, index) => {
-  console.log(item, index);
-});
+console.log(arr[0]);
+console.log(arr[1]);
 
 // Array- Length
 
@@ -18,9 +16,40 @@ console.log(arr.length);
 
 // Array- Adding & Removing Elements
 
-arr.push("orange");           // Adds to the end
-arr.pop();                    // Removes from the end
-arr.unshift("kiwi");          // Adds to the beginning
-arr.shift();                  // Removes from the beginning
-arr.splice(1, 1);             // Removes from the middle
-arr.splice(1, 0, "grape");    // Adds to the middle
+arr.push("orange"); // Adds to the end
+arr.pop(); // Removes from the end
+arr.unshift("kiwi"); // Adds to the beginning
+arr.shift(); // Removes from the beginning
+arr.splice(1, 1); // Removes from the middle
+arr.splice(1, 0, "grape"); // Adds to the middle
+
+// Array- Iterating
+
+// Using for loop
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+// Using while loop
+let i = 0;
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
+}
+// Using for..in
+for (let index in arr) {
+  console.log(index, arr[index]);
+}
+// Using forEach
+arr.forEach((item, index) => {
+  console.log(index, item);
+});
+// Using for..of
+for (let item of arr) {
+  console.log(item);
+}
+
+// Array- Sorting
+
+// Array- Searching
+
+// Array- Slicing & Splicing
