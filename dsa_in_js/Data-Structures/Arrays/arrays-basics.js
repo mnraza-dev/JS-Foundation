6c70713005f93f14f2985ea89f84642b6edaad8a
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-11 17:37:10 +0530

    ✨ add initial array declarations and element access examples

---
+++ dsa_in_js/Data-Structures/Arrays/arrays-basics.js
@@ -0,0 +1,10 @@
+// Arrays declaration
+let arr = [1, 2, 3, 4, 5]; 
+let arr2 = new Array(1, 2, 3, 4, 5); 
+let arr3 = new Array(5); // creates an array of length 5
+let arr4 = new Array(); // creates an empty array
+
+// Accessing elements
+console.log(arr[0]); // 1
+console.log(arr[1]); // 2
+console.log(arr[arr.length - 1]); // 5
