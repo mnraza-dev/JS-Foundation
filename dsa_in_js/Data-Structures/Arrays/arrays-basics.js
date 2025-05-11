d5106a9c6563560c3f585c236b8034dfcfbde312
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-11 17:38:48 +0530

    ✨ update array declarations to use fruit names and enhance element access examples

--- dsa_in_js/Data-Structures/Arrays/arrays-basics.js
+++ dsa_in_js/Data-Structures/Arrays/arrays-basics.js
@@ -1,10 +1,12 @@
 // Arrays declaration
-let arr = [1, 2, 3, 4, 5]; 
-let arr2 = new Array(1, 2, 3, 4, 5); 
-let arr3 = new Array(5); // creates an array of length 5
-let arr4 = new Array(); // creates an empty array
+let arr = ["apple", "banana", "cherry"];
+let arr1 = new Array("apple", "banana", "cherry");
+let arr3 = new Array(5); 
+let arr4 = new Array();
 
 // Accessing elements
-console.log(arr[0]); // 1
-console.log(arr[1]); // 2
-console.log(arr[arr.length - 1]); // 5
+console.log(arr[0]); 
+console.log(arr[1]); 
+arr.map((item, index) => {
+  console.log(item, index);
+});
