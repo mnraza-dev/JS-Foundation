ace748b479be4836635c2e5c9abb79632b40e651
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-13 23:52:09 +0530

    ✨ implement groupAnagrams function to group anagrams together

--- dsa_in_js/Data-Structures/Strings/index.js
+++ dsa_in_js/Data-Structures/Strings/index.js
@@ -172,3 +172,17 @@ console.log(longestPalindrome("cbbd"));   // "bb"
 
 // 💡 Problem 8: Group Anagrams
 // Given an array of strings, group the anagrams together.
+function groupAnagrams(strs) {
+  const map = new Map();
+
+  for (let str of strs) {
+    let sortedStr = str.split('').sort().join('');
+    if (!map.has(sortedStr)) {
+      map.set(sortedStr, []);
+    }
+    map.get(sortedStr).push(str);
+  }
+
+  return Array.from(map.values());
+}
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
