8dcabb44fccb478fc9733d27630db5186d66bcd4
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-13 23:32:48 +0530

    ✨ add example of deleting a property from an object to demonstrate object behavior

--- dsa_in_js/Data-Structures/Objects/index.js
+++ dsa_in_js/Data-Structures/Objects/index.js
@@ -6,3 +6,9 @@ console.log(user?.age ?? "Age not set");
 const user2 = { name: "Bob", age: 25 };
 console.log(user2.age); // 25
 console.log(user2?.age ?? "Age not set");
+
+
+const book = {};
+book.title = "1984";
+delete book.title;
+console.log(book.title); // undefined
