9983e2a7f7543e2371a9da4b1373da39345b6bfe
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-13 23:44:31 +0530

    ✨ add isPalindrome function to check if a string is a palindrome

--- dsa_in_js/Data-Structures/Strings/index.js
+++ dsa_in_js/Data-Structures/Strings/index.js
@@ -23,4 +23,25 @@ console.log(lengthOfLongestSubstring("bbbbb"));    // 1
 console.log(lengthOfLongestSubstring("pwwkew"));   // 3
 
 
+/**
+ * 💡 Problem 2: Check if a String is a Palindrome
+Given a string, check if it is a palindrome (it reads the same backward as forward).
+ */
+
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("hello"));   // false
 
