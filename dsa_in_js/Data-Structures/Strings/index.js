7d433f471a530b4e607f9b58a8af7d6dbabeba4f
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-13 23:43:12 +0530

    ✨ implement lengthOfLongestSubstring function to find the length of the longest substring without repeating characters

---
+++ dsa_in_js/Data-Structures/Strings/index.js
@@ -0,0 +1,19 @@
+// 💡 Problem 1: Longest Substring Without Repeating Characters
+// Given a string, find the length of the longest substring without repeating characters.
+
+function lengthOfLongestSubstring(s) {
+  let map = new Map();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    if (map.has(s[right])) {
+      left = Math.max(map.get(s[right]) + 1, left);
+    }
+    map.set(s[right], right);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
