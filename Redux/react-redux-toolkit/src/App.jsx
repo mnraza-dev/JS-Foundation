bb22f85e0fa2cef77308c999cf8c061e71c7020d
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-09 10:29:03 +0530

    ✨ add incrementByAmount action to counterSlice and update App component for enhanced counter functionality; refactor store import path

--- Redux/react-redux-toolkit/src/App.jsx
+++ Redux/react-redux-toolkit/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { increment, decrement, reset } from "./features/counter/counterSlice";
+import { increment, decrement,incrementByAmount, reset } from "./features/counter/counterSlice";
 const App = () => {
   const count = useSelector((state) => state.counter.value)
   const dispatch = useDispatch()
@@ -10,7 +10,7 @@ const App = () => {
       <button aria-label="Increment value" onClick={() => dispatch(increment())}>+</button>
       <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>-</button>
       <button aria-label="Reset value" onClick={() => dispatch(reset())}>Reset</button>
-
+      <button aria-label="Increment by amount" onClick={() => dispatch(incrementByAmount(10))}>Increment by 10</button>
     </div>
   );
 };
--- Redux/react-redux-toolkit/src/features/counter/counterSlice.js
+++ Redux/react-redux-toolkit/src/features/counter/counterSlice.js
@@ -15,9 +15,12 @@ const counterSlice = createSlice({
         },
         reset : (state) => {
             state.value = 0
+        },
+        incrementByAmount: (state, action) => {
+            state.value += action.payload
         }
     }
 })
 
-export const { increment, decrement, reset } = counterSlice.actions
+export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions
 export default counterSlice.reducer
