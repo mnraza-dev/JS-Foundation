f8c83ff6c59da82e7f142f13cda8255375ec3491
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-09 18:22:58 +0530

    ✨ enhance Form component by adding validation for user input before submission and ensuring proper rendering of user data in a table format

--- Redux/Practices/practice_01/src/comp/Form.jsx
+++ Redux/Practices/practice_01/src/comp/Form.jsx
@@ -1,79 +1,86 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { formData } from "../features/user/userSlice.js";
+
 export default function Form() {
-  const userData = useSelector((state) => state.user.value);
   const dispatch = useDispatch();
+  const userData = useSelector((state) => state.user.value || []);
+
   const [user, setUser] = useState({
     name: "",
     email: "",
     password: "",
   });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(formData(user));
-    setUser({
-      name: "",
-      email: "",
-      password: "",
-    });
+    if (user.name && user.email && user.password) {
+      dispatch(formData(user));
+      setUser({ name: "", email: "", password: "" });
+    }
   };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <h1>Form 😍</h1>
+
         <label htmlFor="name">Name</label>
         <input
-          value={user.name}
-          onChange={(e) => setUser({ ...user, name: e.target.value })}
+          id="name"
           type="text"
           placeholder="Name"
-          id="name"
-        />{" "}
+          value={user.name}
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
         <br />
+
         <label htmlFor="email">Email</label>
         <input
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          id="email"
           type="email"
           placeholder="Email"
-          id="email"
-        />{" "}
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
         <br />
+
         <label htmlFor="password">Password</label>
         <input
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          id="password"
           type="password"
           placeholder="Password"
-          id="password"
-        />{" "}
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
         <br />
+
         <button type="submit">Submit</button>
-        {/* <pre>{JSON.stringify(userData, null, 2)}</pre> */}
       </form>
-      <table
-        style={{ width: "100%", textAlign: "center" }}
-        border={`1px solid #f2f2f2`}
-      >
-        <thead>
-          <tr>
-            <th> Name</th>
-            <th> Email</th>
-            <th> Password</th>
-          </tr>
-        </thead>
-        <tbody>
-          {userData.map((user, index) => (
-            <tr key={index}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.password}</td>
+
+      {Array.isArray(userData) && userData.length > 0 && (
+        <table
+          style={{ width: "100%", textAlign: "center", marginTop: "20px" }}
+          border="1"
+        >
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Password</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {userData.map((user, index) => (
+              <tr key={index}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.password}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 }
