d78cb485ef3d8a21c082d7d1f6278e09bbeaaf18
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-09 10:31:30 +0530

    ✨ enhance App component by adding input for custom increment amount and refactoring button layout for improved readability

--- Redux/react-redux-toolkit/src/App.jsx
+++ Redux/react-redux-toolkit/src/App.jsx
@@ -1,16 +1,46 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { increment, decrement,incrementByAmount, reset } from "./features/counter/counterSlice";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  reset,
+} from "./features/counter/counterSlice";
+import { useState } from "react";
 const App = () => {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
+  const [amount, setAmount] = useState(0);
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Count: {count}</h1>
-      <button aria-label="Increment value" onClick={() => dispatch(increment())}>+</button>
-      <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>-</button>
-      <button aria-label="Reset value" onClick={() => dispatch(reset())}>Reset</button>
-      <button aria-label="Increment by amount" onClick={() => dispatch(incrementByAmount(10))}>Increment by 10</button>
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        +
+      </button>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        -
+      </button>
+      <button aria-label="Reset value" onClick={() => dispatch(reset())}>
+        Reset
+      </button>
+
+      <input
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <button
+        aria-label="Increment by amount"
+        onClick={() => dispatch(incrementByAmount(Number(amount)))}
+      >
+        Inc by Amount
+      </button>
     </div>
   );
 };
