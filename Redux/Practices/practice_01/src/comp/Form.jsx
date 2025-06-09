64e09da7d9a31b0ce43f6326489502e8a3388522
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-09 18:17:10 +0530

    ✨ enhance Form component by restructuring layout to include a table for displaying submitted user data and improving form input organization with line breaks for better readability

--- Redux/Practices/practice_01/src/comp/Form.jsx
+++ Redux/Practices/practice_01/src/comp/Form.jsx
@@ -20,34 +20,60 @@ export default function Form() {
     });
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Form 😍</h1>
-      <label htmlFor="name">Name</label>
-      <input
-        value={user.name}
-        onChange={(e) => setUser({ ...user, name: e.target.value })}
-        type="text"
-        placeholder="Name"
-        id="name"
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
-        type="email"
-        placeholder="Email"
-        id="email"
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
-        type="password"
-        placeholder="Password"
-        id="password"
-      />
-      <button type="submit">Submit</button>
-      <pre>{JSON.stringify(userData, null, 2)}</pre>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>Form 😍</h1>
+        <label htmlFor="name">Name</label>
+        <input
+          value={user.name}
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+          type="text"
+          placeholder="Name"
+          id="name"
+        />{" "}
+        <br />
+        <label htmlFor="email">Email</label>
+        <input
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          type="email"
+          placeholder="Email"
+          id="email"
+        />{" "}
+        <br />
+        <label htmlFor="password">Password</label>
+        <input
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          type="password"
+          placeholder="Password"
+          id="password"
+        />{" "}
+        <br />
+        <button type="submit">Submit</button>
+        {/* <pre>{JSON.stringify(userData, null, 2)}</pre> */}
+      </form>
+      <table
+        style={{ width: "100%", textAlign: "center" }}
+        border={`1px solid #f2f2f2`}
+      >
+        <thead>
+          <tr>
+            <th> Name</th>
+            <th> Email</th>
+            <th> Password</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userData.map((user, index) => (
+            <tr key={index}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.password}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
