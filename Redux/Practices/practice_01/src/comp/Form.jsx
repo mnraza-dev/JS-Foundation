6fa509c8a00d579d71e284bea0551cb982feddd2
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-09 17:39:38 +0530

    ✨ enhance Form component by implementing controlled inputs for user name, email, and password, and adding form submission handling for improved user interaction

--- Redux/Practices/practice_01/src/comp/Form.jsx
+++ Redux/Practices/practice_01/src/comp/Form.jsx
@@ -1,15 +1,43 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Form() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>Form 😍</h1>
       <label htmlFor="name">Name</label>
-      <input type="text" placeholder="Name" id="name" />
+      <input
+        value={user.name}
+        onChange={(e) => setUser({ ...user, name: e.target.value })}
+        type="text"
+        placeholder="Name"
+        id="name"
+      />
       <label htmlFor="email">Email</label>
-      <input type="email" placeholder="Email" id="email" />
+      <input
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        type="email"
+        placeholder="Email"
+        id="email"
+      />
       <label htmlFor="password">Password</label>
-      <input type="password" placeholder="Password" id="password" />
+      <input
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        type="password"
+        placeholder="Password"
+        id="password"
+      />
       <button type="submit">Submit</button>
     </form>
   );
