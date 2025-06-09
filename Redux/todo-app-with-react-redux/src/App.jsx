4b39bcd7f7a9c9bda4b48def9dc1988cff8295fb
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-09 12:48:20 +0530

    ✨ enhance App component by implementing todo addition functionality with input handling and dynamic todo list rendering; refactor layout for improved user interaction

--- Redux/todo-app-with-react-redux/src/App.jsx
+++ Redux/todo-app-with-react-redux/src/App.jsx
@@ -1,24 +1,58 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo } from "./redux/action";
+import { useState } from "react";
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  const filteredTodos = todos.filter((todo) => todo.title);
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    dispatch(addTodo({ id: todos.length + 1, title: input }));
+    setInput("");
+  };
+
   return (
     <>
       <div className="container mx-auto p-4 flex flex-col items-center justify-center">
         <h1 className="text-2xl mb-4 font-bold">
           Todo App with React Redux & Tailwind CSS
         </h1>
-        <input
-          type="text"
-          className="border border-green-300 rounded-md p-2"
-          placeholder="Add a todo"
-        />
-        <button className="bg-blue-500 text-white p-2 rounded-md">
-          Add Todo
-        </button>
-        <ul className="list-disc list-inside">
-          <li>{}</li>
-          <li>Todo 2</li>
-          <li>Todo 3</li>
+        <form onSubmit={handleAddTodo} className="flex gap-2">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            className="border border-green-300 rounded-md p-2"
+            placeholder="Add a todo"
+          />
+          <button
+            type="submit"
+            className="bg-green-500 text-white p-2 rounded-md"
+          >
+            Add Todo
+          </button>
+        </form>
+        <ul className="list-inside">
+          {filteredTodos.map((todo) => (
+            <li
+              className="flex justify-between items-center gap-1"
+              key={todo.id}
+            >
+              <span className="text-lg font-bold">{todo.title}</span>
+              <div className="flex py-2 gap-2">
+                <button className="bg-blue-500 text-white px-4 py-1 rounded-md">
+                  Edit
+                </button>
+                <button className="bg-red-500 text-white px-4 py-1 rounded-md">
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </>
