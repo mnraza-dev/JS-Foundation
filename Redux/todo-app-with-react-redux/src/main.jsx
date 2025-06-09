0cfa753bdaaeb2cdd576a699f16675baec84182e
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-09 12:16:11 +0530

    ✨ integrate Redux into the application by adding react-redux and redux dependencies, and wrap the App component with the Provider for state management.

--- Redux/todo-app-with-react-redux/src/main.jsx
+++ Redux/todo-app-with-react-redux/src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const store = createStore(reducer)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
