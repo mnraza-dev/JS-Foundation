7b5edf91c6c13346715ae49c05e47994b5b4831c
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-09 10:23:23 +0530

    ✨ update dependencies and enhance App component with Redux integration for state management

--- Redux/react-redux-toolkit/src/App.jsx
+++ Redux/react-redux-toolkit/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, reset } from "./features/counter/counterSlice";
 const App = () => {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
   return (
     <div>
-      {/* <h1>Count: {count}</h1> */}
-      <button>Increment</button>
-      <button>Decrement</button>
-      <button>Reset</button>
+      <h1>Count: {count}</h1>
+      <button aria-label="Increment value" onClick={() => dispatch(increment())}>+</button>
+      <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>-</button>
+      <button aria-label="Reset value" onClick={() => dispatch(reset())}>Reset</button>
+
     </div>
   );
 };
