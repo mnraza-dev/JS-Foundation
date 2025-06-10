c22307a5127790794822a48ff2c8b6df23cdb03e
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-10 23:41:14 +0530

    ✨ integrate Tailwind CSS for styling by adding dependencies and updating components for a more modern design, enhancing the overall user interface of the Countdown Timer application

--- ReactJS_Machine_Coding_Round/Countdown-Timer/src/App.jsx
+++ ReactJS_Machine_Coding_Round/Countdown-Timer/src/App.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
   return (
-    <div>
-
-      <h1>Countdown Timer</h1>
+    <div className="h-screen bg-amber-300 flex flex-col items-center justify-center">
+      <h1 className="text-4xl mb-4 font-bold">Countdown Timer</h1>
+      <div className="flex flex-row gap-4">
+        <input
+          className="w-10 h-10 text-center bg-white"
+          type="text"
+          placeholder="HH"
+        />
+        <input
+          className="w-10 h-10 text-center bg-white"
+          type="text"
+          placeholder="MM"
+        />
+        <input
+          className="w-10 h-10 text-center bg-white"
+          type="text"
+          placeholder="SS"
+        />
+      </div>
+      <button
+        className=" mt-4 px-8 bg-fuchsia-800 text-white  py-2 rounded-md"
+        onClick={() => {}}
+      >
+        Start
+      </button>
     </div>
-  )
+  );
 }
