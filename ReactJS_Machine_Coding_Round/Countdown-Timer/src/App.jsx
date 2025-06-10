447030e8882187a9a29e4f544edf47b3b4b37309
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-10 23:46:29 +0530

    ✨ implement countdown timer functionality by adding state management for timer start and enhancing UI with additional controls for pause and reset

--- ReactJS_Machine_Coding_Round/Countdown-Timer/src/App.jsx
+++ ReactJS_Machine_Coding_Round/Countdown-Timer/src/App.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
+  const [isStarted, setIsStarted] = useState(false);
+  // const [hours, setHours] = useState(0);
+  // const [minutes, setMinutes] = useState(0);
+  // const [seconds, setSeconds] = useState(0);
+
+  const handleStart = () => {
+    setIsStarted(true);
+  };
+
   return (
     <div className="h-screen bg-amber-300 flex flex-col items-center justify-center">
       <h1 className="text-4xl mb-4 font-bold">Countdown Timer</h1>
@@ -23,10 +32,28 @@ export default function App() {
       </div>
       <button
         className=" mt-4 px-8 bg-fuchsia-800 text-white  py-2 rounded-md"
-        onClick={() => {}}
+        onClick={() => handleStart()}
       >
         Start
       </button>
+
+      <div>
+        <div className="flex flex-row gap-4">
+          <p>Hours</p>
+          <span>:</span>
+          <p>Minutes</p>
+          <span>:</span>
+          <p>Seconds</p>
+        </div>
+        <div className="flex flex-row gap-4">
+          <button className="bg-fuchsia-800 text-white px-4 py-2 rounded-md">
+            Pause
+          </button>
+          <button className="bg-fuchsia-800 text-white px-4 py-2 rounded-md">
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
