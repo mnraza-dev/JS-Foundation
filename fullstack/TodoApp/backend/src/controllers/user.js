b0ef6aa9e28d95118a1c0761f4c3442ab7001799
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-11 23:28:53 +0530

    ✨ enhance user registration by adding JWT token generation and cookie handling for improved authentication and security

--- fullstack/TodoApp/backend/src/controllers/user.js
+++ fullstack/TodoApp/backend/src/controllers/user.js
@@ -1,20 +1,26 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({ name, email, password: hashedPassword });
-        res.status(201).json({ message: "User created successfully", user });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
-}
-
-
-
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.status(201).json({ message: "User created successfully", user });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
