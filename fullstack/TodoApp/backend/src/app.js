5a385c325af87998678bd3e1ab80292d766433f5
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-12 16:02:47 +0530

    ✨ update CORS configuration

--- fullstack/TodoApp/backend/src/app.js
+++ fullstack/TodoApp/backend/src/app.js
@@ -3,6 +3,8 @@ import { connectDB } from "./db/db.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import todoRouter from "./routes/todo.js";
+
+import cors from cors
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
@@ -10,17 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors(
-  {
-    
-  }
-))
 
-app.get("",(req, res)=>{
-  res.json({
-    message:"Test"
-  })
-})
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use("/api/v1/todo", todoRouter);
 
 app.listen(port, () => {
