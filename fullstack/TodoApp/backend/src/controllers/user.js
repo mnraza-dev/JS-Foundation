a4247b5ff5969d4f6131d5d36e8beee7cce645e1
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-13 23:48:06 +0530

    Add user login functionality in user.js controller and update routes to include login endpoint for enhanced authentication

--- fullstack/TodoApp/backend/src/controllers/user.js
+++ fullstack/TodoApp/backend/src/controllers/user.js
@@ -24,3 +24,28 @@ export const register = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.status(200).json({ message: "Login successful", user });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
