6c8393204970507c2ac7eda6ea4bb31e01aab843
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-18 10:30:13 +0530

    Understood var, let and const to create variables

---
+++ Frontend-Master/JS/basics/app.js
@@ -0,0 +1,26 @@
+var name = "mnraza";
+let my_name = "MN Raza";
+
+function show(){
+    if(1){
+        var funName = "Hey";
+        let localName="there" ;
+    }
+    console.log(funName);
+    console.log(localName);
+}
+show()
+
+console.log(window.name);
+console.log(name);
+console.log(my_name);
+
+// Note: 
+// firstName;
+// If there is no keyword then it will be stored in window object.
+
+// var lastName = "Kumar";  
+// Global/Function/Local Scope
+
+// let/const myNewName = "MN";
+// Block scope created
