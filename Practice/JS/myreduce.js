6cc2e8ef08c191ebaa433c1b70812219f36c55b4
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-26 23:52:41 +0530

    Implement myForEach method for Array prototype

--- Practice/JS/myreduce.js
+++ Practice/JS/myreduce.js
@@ -1,8 +1,6 @@
 Array.prototype.myReduce = function (callback, initialValue) {
   let accumulator = initialValue;
   let startIndex = 0;
-
-  // If initialValue is not given, use the first element
   if (accumulator === undefined) {
     if (this.length === 0) {
       throw new TypeError("Reduce of empty array with no initial value");
@@ -10,15 +8,11 @@ Array.prototype.myReduce = function (callback, initialValue) {
     accumulator = this[0];
     startIndex = 1;
   }
-
   for (let i = startIndex; i < this.length; i++) {
     accumulator = callback(accumulator, this[i], i, this);
   }
-
   return accumulator;
 };
-
 const numbers = [1, 2, 3, 4];
 const total = numbers.myReduce((sum, num) => sum + num, 0);
-
-console.log(total); // 10
+console.log(total);
