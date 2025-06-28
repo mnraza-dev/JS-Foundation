ed65507cd3378788af2b8f0541a28e4f5875be22
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-29 01:47:26 +0530

    adding let/const declarations

--- Frontend-Master/JS/Day_03/hoisting.js
+++ Frontend-Master/JS/Day_03/hoisting.js
@@ -7,3 +7,9 @@ function show() {
     var myAge = 30;
     console.log("Age is: " + myAge);
 }
+
+
+// console.log(myNumber);
+// console.log(address);
+let myNumber = 100;
+const address = "Ranchi, Jharkhand";
