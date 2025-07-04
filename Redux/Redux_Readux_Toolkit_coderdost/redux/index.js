8a3787589ef7e54fc0d1519faae881bbc512edde
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-04 23:58:01 +0530

    feat: enhance reducer with deposit and withdraw actions and log state changes
    - Updated the reducer to handle DEPOSIT and WITHDRAW actions, modifying the amount in the state accordingly
    - Added dispatch calls to demonstrate state changes and log the updated global state after transactions

--- Redux/Redux_Readux_Toolkit_coderdost/redux/index.js
+++ Redux/Redux_Readux_Toolkit_coderdost/redux/index.js
@@ -1,13 +1,33 @@
 import { createStore } from "redux";
 
-// store 
+// store
 const store = createStore(reducer);
 
 // reducer
-function reducer(state={amount:100}, action) {
-   return state;
+function reducer(state = { amount: 100 }, action) {
+  if (action.type === "DEPOSIT") {
+    return {
+      ...state,
+      amount: state.amount + action.payload,
+    };
+  }
+  if (action.type === "WITHDRAW") {
+    return {
+      ...state,
+      amount: state.amount - action.payload,
+    };
+  }
+  return state;
 }
-//global state 
 
-const mydata = store.getState()
-console.log(mydata)
+//global state
+
+const mydata = store.getState();
+console.log(mydata);
+
+// dispatch
+store.dispatch({ type: "DEPOSIT", payload: 100 });
+store.dispatch({ type: "DEPOSIT", payload: 400 });
+
+const mydata2 = store.getState();
+console.log(mydata2);
