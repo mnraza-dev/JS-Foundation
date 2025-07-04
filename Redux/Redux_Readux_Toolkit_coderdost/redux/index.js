fa67f6e080ade852252671e21c824a330fef7195
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-04 23:52:32 +0530

    feat: update reducer to initialize state and log global state
    - Modified the reducer function to set an initial state with amount property
    - Added console log to display the current global state after store creation

--- Redux/Redux_Readux_Toolkit_coderdost/redux/index.js
+++ Redux/Redux_Readux_Toolkit_coderdost/redux/index.js
@@ -3,8 +3,11 @@ import { createStore } from "redux";
 // store 
 const store = createStore(reducer);
 
-
-function reducer(state, action) {
-    
+// reducer
+function reducer(state={amount:100}, action) {
    return state;
 }
+//global state 
+
+const mydata = store.getState()
+console.log(mydata)
