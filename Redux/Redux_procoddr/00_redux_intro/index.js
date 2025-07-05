3b73db3eca43000ccdff3ac8a1d040425aeaa404
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-05 15:46:59 +0530

    refactor: replace string literals with constants for action types

--- Redux/Redux_procoddr/00_redux_intro/index.js
+++ Redux/Redux_procoddr/00_redux_intro/index.js
@@ -2,20 +2,25 @@ import { createStore } from "redux";
 const initialState = {
   count: 0,
 };
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+const INCREMENT_BY_AMOUNT = "incrementByAmount";
+const DECREMENT_BY_AMOUNT = "decrementByAmount";
+const RESET = "reset";
 
 function reducer(state = initialState, action) {
-  if (action.type === "increment") {
+  if (action.type === INCREMENT) {
     return { count: state.count + 1 };
   }
-  if (action.type === "decrement") {
+  if (action.type === DECREMENT) {
     return { count: state.count - 1 };
   }
-  if (action.type === "incrementByAmount") {
+  if (action.type === INCREMENT_BY_AMOUNT) {
     return {
       count: state.count + action.payload,
     };
   }
-   if (action.type === "decrementByAmount") {
+  if (action.type === DECREMENT_BY_AMOUNT) {
     return {
       count: state.count - action.payload,
     };
@@ -34,6 +39,6 @@ store.subscribe(() => {
 
 // store.dispatch({ type: "increment" });
 // store.dispatch({ type: "decrement" });
-store.dispatch({ type: "incrementByAmount", payload: 121 });
-store.dispatch({ type: "decrementByAmount", payload: 11 });
-store.dispatch({ type: "reset" });
+store.dispatch({ type: INCREMENT_BY_AMOUNT, payload: 121 });
+store.dispatch({ type: DECREMENT_BY_AMOUNT, payload: 11 });
+// store.dispatch({ type: RESET });
