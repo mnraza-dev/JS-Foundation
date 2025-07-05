d1b894b52af24bf762786767beda374f0c327361
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-05 14:11:54 +0530

    feat: implement reducer and store setup for increment and decrement actions

--- Redux/Redux_procoddr/00_redux_intro/index.js
+++ Redux/Redux_procoddr/00_redux_intro/index.js
@@ -1,2 +1,20 @@
 import { createStore } from "redux";
+const initialState = {
+  count: 0,
+};
 
+function reducer(state = initialState, action) {
+  if (action.type === "increment") {
+    return { count: state.count + 1 };
+  }
+  if (action.type === "decrement") {
+    return { count: state.count - 1 };
+  }
+  return state;
+}
+
+const store = createStore(reducer);
+console.log(store)
+console.log(store.getState());
+store.dispatch({ type: "increment" });
+console.log(store.getState());
