bfaf56909f03394313c68d39d8f0e76895fd3394
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-05 15:52:00 +0530

    refactor: streamline reducer logic using switch statement

--- Redux/Redux_procoddr/00_redux_intro/index.js
+++ Redux/Redux_procoddr/00_redux_intro/index.js
@@ -1,7 +1,9 @@
 import { createStore } from "redux";
+
 const initialState = {
   count: 0,
 };
+
 const INCREMENT = "increment";
 const DECREMENT = "decrement";
 const INCREMENT_BY_AMOUNT = "incrementByAmount";
@@ -9,36 +11,35 @@ const DECREMENT_BY_AMOUNT = "decrementByAmount";
 const RESET = "reset";
 
 function reducer(state = initialState, action) {
-  if (action.type === INCREMENT) {
-    return { count: state.count + 1 };
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case INCREMENT_BY_AMOUNT:
+      return {
+        ...state,
+        count: state.count + action.payload,
+      };
+    case DECREMENT_BY_AMOUNT:
+      return {
+        ...state,
+        count: state.count - action.payload,
+      };
+    case RESET:
+      return { count: (state.count = 0) };
+    default:
+      return state;
   }
-  if (action.type === DECREMENT) {
-    return { count: state.count - 1 };
-  }
-  if (action.type === INCREMENT_BY_AMOUNT) {
-    return {
-      count: state.count + action.payload,
-    };
-  }
-  if (action.type === DECREMENT_BY_AMOUNT) {
-    return {
-      count: state.count - action.payload,
-    };
-  }
-  if (action.type === "reset") {
-    return { count: (state.count = 0) };
-  }
-  return state;
 }
-
 const store = createStore(reducer);
 
 store.subscribe(() => {
   console.log("State changed:", store.getState());
 });
 
-// store.dispatch({ type: "increment" });
-// store.dispatch({ type: "decrement" });
+// store.dispatch({ type: INCREMENT });
+// store.dispatch({ type: DECREMENT });
 store.dispatch({ type: INCREMENT_BY_AMOUNT, payload: 121 });
 store.dispatch({ type: DECREMENT_BY_AMOUNT, payload: 11 });
 // store.dispatch({ type: RESET });
