d85bf4e023b27c77635ff4778029826707c22b8c
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-25 02:27:27 +0530

    refactor: streamline data type examples and enhance console output for clarity

--- Practice/JS/datatypes.js
+++ Practice/JS/datatypes.js
@@ -19,44 +19,27 @@ console.log(typeof price)
 // Null 
 let first_name = null
 console.log(first_name)
+console.log(typeof first_name)
 
 // Undefined
 let name;
 console.log(name)
+console.log(typeof name)
 
 // Boolean
 let isRaining = true
 console.log(isRaining)
 console.log(typeof isRaining)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// String
 console.log(typeof "Raza")
+
+
+
 console.log('first'+' last')
 console.log(7+8)
 console.log("4"+2)
+console.log("4"-2)
 console.log(2+"4")
 console.log(2 + +"4")
 console.log(2 - +"4")
