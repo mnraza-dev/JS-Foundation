ecc402f0e4eb02c01e91c54e6b172009b9fb8879
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-25 02:17:08 +0530

    feat: enhance variable demonstration with additional examples and comments

---
+++ Practice/JS/datatypes.js
@@ -0,0 +1,23 @@
+/**
+ * Datatypes in JS
+ * 
+ * Number 
+ * Null
+ * String
+ * Symbol
+ * Boolean
+ * BigInt
+ * Undefined
+ *
+ */
+
+console.log(typeof "Raza")
+console.log('first'+' last')
+console.log(7+8)
+console.log("4"+2)
+console.log(2+"4")
+console.log(2 + +"4")
+console.log(2 - +"4")
+console.log(12 - +"4")
+console.log(12 - -"4")
+console.log(12 + -"4")
--- Practice/JS/variables.js
+++ Practice/JS/variables.js
@@ -1,7 +1,8 @@
 // This is a simple JavaScript code snippet demonstrating variable declaration and usage.
 // Variables can be declared using let, const, or var.
 // let is block-scoped, const is also block-scoped but cannot be reassigned, and var is function-scoped.
-
+let name ;
+console.log(name)
 let a = 10;
 let b = 20;
 let c = a + b;
@@ -11,8 +12,16 @@ const PI = 3.14;
 let radius = 5;
 let area = PI * radius * radius;
 
+// PI = 3.14159; // This will throw an error because PI is a constant and cannot be reassigned.
+// console.log(PI);
+
 console.log("Area of the circle: " + area);
 
 console.log(age);
 
 console.log(c);
+
+
+// Rules :
+// 1. Variables declared with let and const are block-scoped.
+
